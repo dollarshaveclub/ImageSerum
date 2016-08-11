c61773df18da358b8fc3bb416d88a5c3c5af4dc2
David Peredo <daveperedo@gmail.com>
2016-08-11 13:16:40 -0700

    Initial commit.

---
+++ ImageSerum/ImageSerum.h
@@ -0,0 +1,19 @@
+//
+//  ImageSerum.h
+//  ImageSerum
+//
+//  Created by David Peredo on 8/9/16.
+//  Copyright © 2016 Dollar Shave Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageSerum.
+FOUNDATION_EXPORT double ImageSerumVersionNumber;
+
+//! Project version string for ImageSerum.
+FOUNDATION_EXPORT const unsigned char ImageSerumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageSerum/PublicHeader.h>
+
+
