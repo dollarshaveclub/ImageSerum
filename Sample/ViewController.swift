220680cf0db59f4f24aecb8b1ac3ffe1011c4a2f
Michael Mork <morkrom@protonmail.ch>
2017-01-19 21:52:17 -0800

    WebP available as module in Swift

---
+++ Sample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Michael Mork on 1/19/17.
+//  Copyright © 2017 Dollar Shave Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    
+    }
+
+
+}
+
